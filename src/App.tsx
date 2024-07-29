e47e96424b7a67d69ff76dee4b596edfbdffcad4
taynara veloso <taynarasantos2.3@hotmail.com>
2024-07-28 22:42:43 -0300

    :star: App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/transactions'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <Transactions />
+   </ThemeProvider>
+  )
+}
+
+export default App
