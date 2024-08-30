79d07519e8291bfc962de5caa0f18b9390d0575b
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-30 19:23:41 -0300

    :star: add TransactionsProvider in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { ThemeProvider } from 'styled-components'
 import { Transactions } from './pages/transactions'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { TransactionsProvider } from './contexts/TransactionContext'
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
    </ThemeProvider>
   )
 }
