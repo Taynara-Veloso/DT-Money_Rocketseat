6becebab33aaad98f2d0afa603b0a93f0e3a9def
taynara veloso <taynarasantos2.3@hotmail.com>
2024-09-03 20:19:00 -0300

    :star: import * as z

--- src/pages/transactions/components/SearchForm/index.tsx
+++ src/pages/transactions/components/SearchForm/index.tsx
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import * as z  from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { MagnifyingGlass } from "phosphor-react";
